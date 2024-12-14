ee3fe7d9588b9398874b823fb09653de838a91b9
ShapovalovIlia <ilia-shapvalov04@mail.ru>
2024-12-14 22:00:06 +0300

    changed repo structure

--- students/k3339/Shapovlaov_Ilia/practical_works/2.1/django_project_shapovalov/project_first_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import AutoOwner, Auto, Own, DriversLicense
-
-admin.site.register([AutoOwner, Auto, Own, DriversLicense])
--- students/k3339/Shapovlaov_Ilia/practical_works/2.1/django_project_shapovalov/project_first_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from project_first_app import views
-
-urlpatterns = [
-    path('auto_owner/<int:poll_id>/', views.auto_owner_info, name='auto_owner_info'),
-]
